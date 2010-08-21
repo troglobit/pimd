a7dc70539df74d72ef9b0b42672320564f57b889
Joachim Nilsson <troglobit@vmlinux.org>
2010-08-21 15:47:00 +0200

    Update OpenBSD ip_mroute.h, used if missing in earlier releases. Add README.

--- include/openbsd/netinet/ip_mroute.h
+++ include/openbsd/netinet/ip_mroute.h
@@ -1,5 +1,8 @@
-/*	$OpenBSD: ip_mroute.h,v 1.6 2000/01/21 03:15:05 angelos Exp $	*/
-/*	$NetBSD: ip_mroute.h,v 1.10 1996/02/13 23:42:55 christos Exp $	*/
+/*	$OpenBSD: ip_mroute.h,v 1.15 2009/07/13 19:14:29 michele Exp $	*/
+/*	$NetBSD: ip_mroute.h,v 1.23 2004/04/21 17:49:46 itojun Exp $	*/
+
+#ifndef _NETINET_IP_MROUTE_H_
+#define _NETINET_IP_MROUTE_H_
 
 /*
  * Definitions for IP multicast forwarding.
@@ -9,11 +12,15 @@
  * Modified by Ajit Thyagarajan, PARC, August 1993.
  * Modified by Ajit Thyagarajan, PARC, August 1994.
  * Modified by Ahmed Helmy, SGI, June 1996.
+ * Modified by Pavlin Radoslavov, ICSI, October 2002.
  *
  * MROUTING Revision: 1.2
+ * and PIM-SMv2 and PIM-DM support, advanced API support,
+ * bandwidth metering and signaling.
  */
 
 #include <sys/queue.h>
+#include <sys/timeout.h>
 
 /*
  * Multicast Routing set/getsockopt commands.
@@ -25,7 +32,12 @@
 #define	MRT_ADD_MFC		104	/* insert forwarding cache entry */
 #define	MRT_DEL_MFC		105	/* delete forwarding cache entry */
 #define	MRT_VERSION		106	/* get kernel version number */
-#define	MRT_ASSERT		107	/* enable assert (wrong iif) processing */
+#define	MRT_ASSERT		107	/* enable assert processing */
+#define	MRT_PIM			MRT_ASSERT /* enable PIM processing */
+#define	MRT_API_SUPPORT		109	/* supported MRT API */
+#define	MRT_API_CONFIG		110	/* config MRT API */
+#define	MRT_ADD_BW_UPCALL	111	/* create bandwidth monitor */
+#define	MRT_DEL_BW_UPCALL	112	/* delete bandwidth monitor */
 
 
 /*
@@ -45,7 +57,7 @@ typedef u_int16_t vifi_t;		/* type of a vif index */
 
 #define	VIFF_TUNNEL	0x1		/* vif represents a tunnel end-point */
 #define	VIFF_SRCRT	0x2		/* tunnel uses IP src routing */
-#define VIFF_REGISTER	0x4		/* vif used for Register encap/decap */
+#define	VIFF_REGISTER	0x4		/* used for PIM Register encap/decap */
 
 /*
  * Argument structure for MRT_ADD_VIF.
@@ -53,16 +65,16 @@ typedef u_int16_t vifi_t;		/* type of a vif index */
  */
 struct vifctl {
 	vifi_t	  vifc_vifi;	    	/* the index of the vif to be added */
-	u_int8_t  vifc_flags;     	/* VIFF_ flags defined below */
+	u_int8_t  vifc_flags;     	/* VIFF_ flags defined above */
 	u_int8_t  vifc_threshold; 	/* min ttl required to forward on vif */
-	u_int32_t vifc_rate_limit;	/* max rate */
+	u_int32_t vifc_rate_limit;	/* ignored */
 	struct	  in_addr vifc_lcl_addr;/* local interface address */
 	struct	  in_addr vifc_rmt_addr;/* remote address (tunnels only) */
 };
 
 /*
  * Argument structure for MRT_ADD_MFC and MRT_DEL_MFC.
- * (mfcc_tos to be added at a future point)
+ * XXX if you change this, make sure to change struct mfcctl2 as well.
  */
 struct mfcctl {
 	struct	 in_addr mfcc_origin;	/* ip origin of mcasts */
@@ -70,7 +82,95 @@ struct mfcctl {
 	vifi_t	 mfcc_parent;		/* incoming vif */
 	u_int8_t mfcc_ttls[MAXVIFS];	/* forwarding ttls on vifs */
 };
-  
+
+/*
+ * The new argument structure for MRT_ADD_MFC and MRT_DEL_MFC overlays
+ * and extends the old struct mfcctl.
+ */
+struct mfcctl2 {
+	/* the mfcctl fields */
+	struct in_addr	mfcc_origin;		/* ip origin of mcasts	     */
+	struct in_addr	mfcc_mcastgrp;		/* multicast group associated*/
+	vifi_t		mfcc_parent;		/* incoming vif		     */
+	u_int8_t	mfcc_ttls[MAXVIFS]; 	/* forwarding ttls on vifs   */
+
+	/* extension fields */
+	u_int8_t	mfcc_flags[MAXVIFS];	/* the MRT_MFC_FLAGS_* flags */
+	struct in_addr	mfcc_rp;		/* the RP address            */
+};
+/*
+ * The advanced-API flags.
+ *
+ * The MRT_MFC_FLAGS_XXX API flags are also used as flags
+ * for the mfcc_flags field.
+ */
+#define	MRT_MFC_FLAGS_DISABLE_WRONGVIF	(1 << 0) /* disable WRONGVIF signals */
+#define	MRT_MFC_FLAGS_BORDER_VIF	(1 << 1) /* border vif		     */
+#define	MRT_MFC_RP			(1 << 8) /* enable RP address	     */
+#define	MRT_MFC_BW_UPCALL		(1 << 9) /* enable bw upcalls	     */
+#define	MRT_MFC_FLAGS_ALL		(MRT_MFC_FLAGS_DISABLE_WRONGVIF |    \
+					 MRT_MFC_FLAGS_BORDER_VIF)
+#define	MRT_API_FLAGS_ALL		(MRT_MFC_FLAGS_ALL |		     \
+					 MRT_MFC_RP |			     \
+					 MRT_MFC_BW_UPCALL)
+
+/*
+ * Structure for installing or delivering an upcall if the
+ * measured bandwidth is above or below a threshold.
+ *
+ * User programs (e.g. daemons) may have a need to know when the
+ * bandwidth used by some data flow is above or below some threshold.
+ * This interface allows the userland to specify the threshold (in
+ * bytes and/or packets) and the measurement interval. Flows are
+ * all packet with the same source and destination IP address.
+ * At the moment the code is only used for multicast destinations
+ * but there is nothing that prevents its use for unicast.
+ *
+ * The measurement interval cannot be shorter than some Tmin (currently, 3s).
+ * The threshold is set in packets and/or bytes per_interval.
+ *
+ * Measurement works as follows:
+ *
+ * For >= measurements: 
+ * The first packet marks the start of a measurement interval.
+ * During an interval we count packets and bytes, and when we
+ * pass the threshold we deliver an upcall and we are done.
+ * The first packet after the end of the interval resets the
+ * count and restarts the measurement.
+ *
+ * For <= measurement:
+ * We start a timer to fire at the end of the interval, and
+ * then for each incoming packet we count packets and bytes.
+ * When the timer fires, we compare the value with the threshold,
+ * schedule an upcall if we are below, and restart the measurement
+ * (reschedule timer and zero counters).
+ */
+
+struct bw_data {
+	struct timeval	b_time;
+	u_int64_t	b_packets;
+	u_int64_t	b_bytes;
+};
+
+struct bw_upcall {
+	struct in_addr	bu_src;			/* source address            */
+	struct in_addr	bu_dst;			/* destination address       */
+	u_int32_t	bu_flags;		/* misc flags (see below)    */
+#define	BW_UPCALL_UNIT_PACKETS   (1 << 0)	/* threshold (in packets)    */
+#define	BW_UPCALL_UNIT_BYTES     (1 << 1)	/* threshold (in bytes)      */
+#define	BW_UPCALL_GEQ            (1 << 2)	/* upcall if bw >= threshold */
+#define	BW_UPCALL_LEQ            (1 << 3)	/* upcall if bw <= threshold */
+#define	BW_UPCALL_DELETE_ALL     (1 << 4)	/* delete all upcalls for s,d*/
+	struct bw_data	bu_threshold;		/* the bw threshold	     */
+	struct bw_data	bu_measured;		/* the measured bw	     */
+};
+
+/* max. number of upcalls to deliver together */
+#define	BW_UPCALLS_MAX				128
+/* min. threshold time interval for bandwidth measurement */
+#define	BW_UPCALL_THRESHOLD_INTERVAL_MIN_SEC	3
+#define	BW_UPCALL_THRESHOLD_INTERVAL_MIN_USEC	0
+
 /*
  * Argument structure used by mrouted to get src-grp pkt counts.
  */
@@ -81,7 +181,7 @@ struct sioc_sg_req {
 	u_long	bytecnt;
 	u_long	wrong_if;
 };
-  
+
 /*
  * Argument structure used by mrouted to get vif pkt counts.
  */
@@ -112,18 +212,9 @@ struct mrtstat {
 	u_long	mrts_pkt2large;     	/* pkts dropped - size > BKT SIZE */
 	u_long	mrts_upq_sockfull;	/* upcalls dropped - socket full */
 };
-  
+
 
 #ifdef _KERNEL
-
-/*
- * Token bucket filter at each vif
- */
-struct tbf {
-	u_int32_t last_pkt_t;		/* arr. time of last pkt */
-	u_int32_t n_tok;		/* no of tokens in bucket */
-	u_int32_t q_len;		/* length of queue at this vif */
-};
 
 /*
  * The kernel's virtual-interface structure.
@@ -131,8 +222,6 @@ struct tbf {
 struct vif {
 	u_int8_t  v_flags;		/* VIFF_ flags defined above */
 	u_int8_t  v_threshold;		/* min ttl required to forward on vif */
-	u_int32_t v_rate_limit;		/* max rate */
-	struct	  tbf v_tbf;		/* token bucket structure at intf. */
 	struct	  in_addr v_lcl_addr;	/* local interface address */
 	struct	  in_addr v_rmt_addr;	/* remote address (tunnels only) */
 	struct	  ifnet *v_ifp;		/* pointer to interface */
@@ -141,6 +230,7 @@ struct vif {
 	u_long	  v_bytes_in;		/* # bytes in on interface */
 	u_long	  v_bytes_out;		/* # bytes out on interface */
 	struct	  route v_route;	/* cached route if this is a tunnel */
+	struct	  timeout v_repq_ch;	/* for tbf_reprocess_q() */
 #ifdef RSVP_ISI
 	int	  v_rsvp_on;		/* # RSVP listening on this vif */
 	struct	  socket *v_rsvpd;	/* # RSVPD daemon */
@@ -149,7 +239,7 @@ struct vif {
 
 /*
  * The kernel's multicast forwarding cache entry structure.
- * (A field for the type of service (mfc_tos) is to be added 
+ * (A field for the type of service (mfc_tos) is to be added
  * at a future point.)
  */
 struct mfc {
@@ -164,6 +254,9 @@ struct mfc {
 	int	 mfc_expire;			/* time to clean entry up */
 	struct	 timeval mfc_last_assert;	/* last time I sent an assert */
 	struct	 rtdetq *mfc_stall;		/* pkts waiting for route */
+	u_int8_t mfc_flags[MAXVIFS];		/* the MRT_MFC_FLAGS_* flags */
+	struct in_addr	mfc_rp;			/* the RP address	     */
+	struct bw_meter	*mfc_bw_meter;		/* list of bandwidth meters  */
 };
 
 /*
@@ -174,9 +267,10 @@ struct igmpmsg {
 	u_int32_t unused1;
 	u_int32_t unused2;
 	u_int8_t  im_msgtype;		/* what type of message */
-#define IGMPMSG_NOCACHE		1
-#define IGMPMSG_WRONGVIF	2
-#define IGMPMSG_WHOLEPKT	3
+#define	IGMPMSG_NOCACHE		1	/* no MFC in the kernel		    */
+#define	IGMPMSG_WRONGVIF	2	/* packet came from wrong interface */
+#define	IGMPMSG_WHOLEPKT	3	/* PIM pkt for user level encap.    */
+#define	IGMPMSG_BW_UPCALL	4	/* BW monitoring upcall		    */
 	u_int8_t  im_mbz;		/* must be zero */
 	u_int8_t  im_vif;		/* vif rec'd on */
 	u_int8_t  unused3;
@@ -197,42 +291,50 @@ struct rtdetq {
 
 #define	MFCTBLSIZ	256
 #define	MAX_UPQ		4		/* max. no of pkts in upcall Q */
-  
+
 /*
- * Token bucket filter code 
+ * Structure for measuring the bandwidth and sending an upcall if the
+ * measured bandwidth is above or below a threshold.
  */
-#define	MAX_BKT_SIZE    10000		/* 10K bytes size */
-#define	MAXQSIZE        10		/* max. no of pkts in token queue */
-  
-/*
- * Queue structure at each vif
- */
-struct pkt_queue {
-	u_int32_t pkt_len;		/* length of packet in queue */
-	struct	  mbuf *pkt_m;		/* pointer to packet mbuf */
-	struct	  ip *pkt_ip;		/* pointer to ip header */
+struct bw_meter {
+	struct bw_meter	*bm_mfc_next;		/* next bw meter (same mfc)  */
+	struct bw_meter	*bm_time_next;		/* next bw meter (same time) */
+	uint32_t	bm_time_hash;		/* the time hash value       */
+	struct mfc	*bm_mfc;		/* the corresponding mfc     */
+	uint32_t	bm_flags;		/* misc flags (see below)    */
+#define	BW_METER_UNIT_PACKETS	(1 << 0)	/* threshold (in packets)    */
+#define	BW_METER_UNIT_BYTES	(1 << 1)	/* threshold (in bytes)      */
+#define	BW_METER_GEQ		(1 << 2)	/* upcall if bw >= threshold */
+#define	BW_METER_LEQ		(1 << 3)	/* upcall if bw <= threshold */
+#define	BW_METER_USER_FLAGS 	(BW_METER_UNIT_PACKETS |		\
+				 BW_METER_UNIT_BYTES |			\
+				 BW_METER_GEQ |				\
+				 BW_METER_LEQ)
+
+#define	BW_METER_UPCALL_DELIVERED (1 << 24)	/* upcall was delivered      */
+
+	struct bw_data	bm_threshold;		/* the upcall threshold	     */
+	struct bw_data	bm_measured;		/* the measured bw	     */
+	struct timeval	bm_start_time;		/* abs. time		     */
 };
-  
-int	ip_mrouter_set __P((int, struct socket *, struct mbuf **));
-int	ip_mrouter_get __P((int, struct socket *, struct mbuf **));
-int	mrt_ioctl __P((u_long, caddr_t));
-int	ip_mrouter_done __P((void));
-void	reset_vif __P((struct vif *));
-void	vif_delete __P((struct ifnet *));
+
+int	ip_mrouter_set(struct socket *, int, struct mbuf **);
+int	ip_mrouter_get(struct socket *, int, struct mbuf **);
+int	mrt_ioctl(struct socket *, u_long, caddr_t);
+int	ip_mrouter_done(void);
+void	ip_mrouter_detach(struct ifnet *);
+void	reset_vif(struct vif *);
+void	vif_delete(struct ifnet *);
 #ifdef RSVP_ISI
-int	ip_mforward __P((struct mbuf *, struct ifnet *, struct ip_moptions *));
-int	legal_vif_num __P((int));
-int	ip_rsvp_vif_init __P((struct socket *, struct mbuf *));
-int	ip_rsvp_vif_done __P((struct socket *, struct mbuf *));
-void	ip_rsvp_force_done __P((struct socket *));
-#if 0
-void	rsvp_input __P((struct mbuf *, struct ifnet *));
+int	ip_mforward(struct mbuf *, struct ifnet *, struct ip_moptions *);
+int	legal_vif_num(int);
+int	ip_rsvp_vif_init(struct socket *, struct mbuf *);
+int	ip_rsvp_vif_done(struct socket *, struct mbuf *);
+void	ip_rsvp_force_done(struct socket *);
+void	rsvp_input(struct mbuf *, int, int);
 #else
-void rsvp_input __P((struct mbuf *, int, int));
-#endif
-#else
-int	ip_mforward __P((struct mbuf *, struct ifnet *));
-#endif
-void	ipip_mroute_input __P((struct mbuf *, ...));
+int	ip_mforward(struct mbuf *, struct ifnet *);
+#endif /* RSVP_ISI */
 
 #endif /* _KERNEL */
+#endif /* _NETINET_IP_MROUTE_H_ */
