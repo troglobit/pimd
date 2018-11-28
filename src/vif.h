c2735d05db1b829db9b123a6217e5ed1493e566c
wgc1212 <44960622+wgc1212@users.noreply.github.com>
2018-11-28 00:03:23 -0500

    Fix #131: Regression in last hop router compared to v2.3.2.  Transition from bit array to int array comparison.

--- src/vif.h
+++ src/vif.h
@@ -78,7 +78,7 @@ inline static void PIMD_VIFM_MERGE(uint8_t *m1, uint8_t *m2, uint8_t *result)
 	result[n] = m1[n] | m2[n];
 }
 
-/* Check whether I am the last hop on some LAN */
+/* Check with any oifs whether I am the last hop on some LAN */
 inline static int PIMD_VIFM_LASTHOP_ROUTER(uint8_t *leaves, uint8_t *oifs)
 {
     int i;
