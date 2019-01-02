b7a93d42c9a1cd4375dd0e4fb1bebe84c6dfda15
Joachim Nilsson <troglobit@gmail.com>
2019-01-02 20:00:09 +0100

    timer.c:age_vifs() - Minor, coding style fix
    Assignments and increments inside if()-statements are bad form and also
    make the code less readable.
    
    Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

--- src/timer.c
+++ src/timer.c
@@ -170,20 +170,22 @@ void age_vifs(void)
 	IF_TIMEOUT(v->uv_gq_timer)
 	    query_groups(v);
 
-	if (v->uv_querier &&
-	    (v->uv_querier->al_timer += TIMER_INTERVAL) > igmp_querier_timeout) {
-	    /*
-	     * The current querier has timed out.  We must become the
-	     * querier.
-	     */
-	    IF_DEBUG(DEBUG_IGMP) {
-		logit(LOG_DEBUG, 0, "IGMP Querier %s timed out.",
-		      inet_fmt(v->uv_querier->al_addr, s1, sizeof(s1)));
+	if (v->uv_querier)
+	    v->uv_querier->al_timer += TIMER_INTERVAL;
+	    if (v->uv_querier->al_timer > igmp_querier_timeout) {
+		/*
+		 * The current querier has timed out.  We must become
+		 * the querier.
+		 */
+		IF_DEBUG(DEBUG_IGMP) {
+		    logit(LOG_DEBUG, 0, "IGMP Querier %s timed out.",
+			  inet_fmt(v->uv_querier->al_addr, s1, sizeof(s1)));
+		}
+		free(v->uv_querier);
+		v->uv_querier = NULL;
+		v->uv_flags |= VIFF_QUERIER;
+		query_groups(v);
 	    }
-	    free(v->uv_querier);
-	    v->uv_querier = NULL;
-	    v->uv_flags |= VIFF_QUERIER;
-	    query_groups(v);
 	}
     }
 
