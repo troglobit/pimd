troglobit/pimd

commit bdf546c94410df8c4a38ff72e7248cc4929125c3

Refactor openlog() and log to stderr if running in foreground
Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/debug.c
@@ -388,6 +388,23 @@ void dump_vifs(FILE *fp)
     fprintf(fp, "\n\n");
 }
 
+void log_init(int log_stdout)
+{
+    int log_opts = LOG_PID;
+
+#ifdef LOG_PERROR
+    if (log_stdout)
+	log_opts |= LOG_PERROR;
+#endif
+
+#ifdef LOG_DAEMON
+    openlog(ident, log_opts, LOG_DAEMON);
+    setlogmask(LOG_UPTO(loglevel));
+#else
+    openlog(ident, log_opts);
+#endif
+}
+
 int loglvl(char *level)
 {
     int i;

src/defs.h
@@ -465,6 +465,7 @@ extern void	config_vifs_from_file	(void);
 /* debug.c */
 extern char	*packet_kind		(int proto, int type, int code);
 extern int	debug_kind		(int proto, int type, int code);
+extern void	log_init		(int log_stdout);
 extern void	logit			(int, int, const char *, ...);
 extern void	dump_frame		(char *desc, void *dump, size_t len);
 extern int	log_level		(int proto, int type, int code);

src/main.c
@@ -457,13 +457,7 @@ int main(int argc, char *argv[])
     /*
      * Setup logging
      */
-#ifdef LOG_DAEMON
-    openlog(ident, LOG_PID, LOG_DAEMON);
-    setlogmask(LOG_UPTO(loglevel));
-#else
-    openlog(ident, LOG_PID);
-#endif /* LOG_DAEMON */
-
+    log_init(haveterminal);
     logit(LOG_NOTICE, 0, "%s starting ...", versionstring);
 
     do_randomize();

commit 4bb9fb25d1d8029f70066046b0e8cceabb6c2b02

Make pimd behave like a normal UNIX daemon wrt logging to syslog/TTY
This patch removes all the (now) superfluous logging to stderr when
running in debug mode, we log state changes and the user has the new
pimctl tool for diagnostics.  Only -n enables logging to stdout/stderr,
wheres syslog is enabled by default, and when -s is explicitly given.

Also fixes a regression in logging to the TTY introduced in bdf546c by
removing the non-portable LOG_PERROR flag to openlog().

Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

man/pimd.8
@@ -84,16 +84,7 @@ and
 .Cm rp-candidate
 are commented out.
 .It Fl d, -debug Ar SYS[,SYS...]
-By default,
-.Nm
-daemonizes itself by detaching from the invoking terminal and forking to
-the background.  However, if
-.Fl d
-or
-.Fl n, -foreground
-is specified,
-.Nm
-runs in the foreground of the starting terminal.  If
+This option controls extended subsystem debugging.  If
 .Fl d
 is given without any argument
 .Nm
@@ -143,7 +134,11 @@ Debug hello messages to/from neighbors
 Run in the foreground, do not detach from calling terminal and do not
 fork to background.  Useful not only when debugging (above) but also
 when running under a process monitor like daemontools, runit, finit, or
-systemd.
+systemd.  Remember to also use
+.Fl s ,
+to send all logs to the syslog daemon, unless you want to debug
+.Nm
+and run in the foreground.
 .It Fl i, Fl -ident Ar NAME
 Specify program identity (name) to be used for configuration file, PID
 file and syslog messages.  Useful with multiple instances of

src/debug.c
@@ -129,6 +129,7 @@ unsigned long debug = 0x00000000;        /* If (long) is smaller than
 					  * 4 bytes, then we are in
 					  * trouble.
 					  */
+static int log_syslog = 0;
 
 
 char *packet_kind(int proto, int type, int code)
@@ -342,8 +343,7 @@ int debug_parse(char *arg)
  * "No route to host").  This is important when there is asymmetric
  * reachability and someone is trying to, i.e., mrinfo me periodically.
  */
-int
-log_level(int proto, int type, int code)
+int log_level(int proto, int type, int code)
 {
     switch (proto) {
 	case IPPROTO_IGMP:
@@ -486,14 +486,11 @@ void dump_ssm(FILE *fp)
     }
 }
 
-void log_init(int log_stdout)
+void log_init(int do_syslog)
 {
     int log_opts = LOG_PID;
 
-#ifdef LOG_PERROR
-    if (log_stdout)
-	log_opts |= LOG_PERROR;
-#endif
+    log_syslog = do_syslog;
 
 #ifdef LOG_DAEMON
     openlog(ident, log_opts, LOG_DAEMON);
@@ -560,11 +557,11 @@ void logit(int severity, int syserr, const char *format, ...)
     vsnprintf(msg, sizeof(msg), format, ap);
     va_end(ap);
 
-    /*
-     * Log to stderr if we haven't forked yet and it's a warning or
-     * worse, or if we're debugging.
-     */
-    if (haveterminal && (debug || severity <= LOG_WARNING)) {
+    /* pimd running in foreground */
+    if (!log_syslog) {
+	if (!debug && severity > loglevel)
+	    return;
+
 	gettimeofday(&now, NULL);
 	lt = now.tv_sec;
 	thyme = localtime(&lt);
@@ -579,6 +576,7 @@ void logit(int severity, int syserr, const char *format, ...)
 	    fprintf(stderr, ": %s", strerror(syserr));
 
 	fprintf(stderr, "\n");
+	return;
     }
 
     /*

src/debug.h
@@ -110,8 +110,6 @@ extern char	*packet_kind		(int proto, int type, int code);
 extern int	debug_kind		(int proto, int type, int code);
 extern int      debug_list              (int mask, char *buf, size_t len);
 extern int      debug_parse             (char *arg);
-extern void	log_init		(int log_stdout);
-extern void	logit			(int, int, const char *, ...);
 extern void	dump_frame		(char *desc, void *dump, size_t len);
 extern int	log_level		(int proto, int type, int code);
 extern void	dump_vifs		(FILE *fp);
@@ -119,8 +117,11 @@ extern void	dump_ssm		(FILE *fp);
 extern void	dump_pim_mrt		(FILE *fp);
 extern int	dump_rp_set		(FILE *fp);
 
+extern void	log_init		(int do_syslog);
 extern int      log_str2lvl             (char *level);
 extern char    *log_lvl2str             (int val);
 extern int      log_list                (char *buf, size_t len);
+
+extern void	logit			(int severity, int syserr, const char *fmt, ...);
 
 #endif /* PIMD_DEBUG_H_ */

src/defs.h
@@ -302,7 +302,6 @@ extern int		build_jp_message_pool_counter;
 extern uint32_t		virtual_time;
 extern char            *ident;
 extern char	       *config_file;
-extern int              haveterminal;
 extern char            *prognm;
 
 extern struct cand_rp_adv_message_ {

src/main.c
@@ -50,7 +50,6 @@ char versionstring[100];
 int do_vifs       = 1;
 int no_fallback   = 0;
 int retry_forever = 0;
-int haveterminal  = 1;
 struct rp_hold *g_rp_hold = NULL;
 int mrt_table_id = 0;
 
@@ -250,7 +249,7 @@ int main(int argc, char *argv[])
 	{ NULL, 0, 0, 0 }
     };
 
-    snprintf(versionstring, sizeof (versionstring), "pimd version %s", PACKAGE_VERSION);
+    snprintf(versionstring, sizeof(versionstring), "pimd version %s", PACKAGE_VERSION);
 
     prognm = ident = progname(argv[0]);
     while ((ch = getopt_long(argc, argv, "d:f:hi:l:nrst:v", long_options, NULL)) != EOF) {
@@ -340,9 +339,8 @@ int main(int argc, char *argv[])
 	printf("debug level 0x%lx (%s)\n", debug, buf);
     }
 
-    if (!debug && !foreground) {
+    if (!foreground) {
 	/* Detach from the terminal */
-	haveterminal = 0;
 	if (fork())
 	    exit(0);
 
@@ -382,7 +380,7 @@ int main(int argc, char *argv[])
     /*
      * Setup logging
      */
-    log_init(haveterminal && do_syslog > 0);
+    log_init(do_syslog);
     logit(LOG_NOTICE, 0, "%s starting ...", versionstring);
 
     do_randomize();
@@ -416,11 +414,6 @@ int main(int argc, char *argv[])
     sigaction(SIGINT, &sa, NULL);
     sigaction(SIGUSR1, &sa, NULL);
     sigaction(SIGUSR2, &sa, NULL);
-
-    IF_DEBUG(DEBUG_IF)
-	dump_vifs(stderr);
-    IF_DEBUG(DEBUG_PIM_MRT)
-	dump_pim_mrt(stderr);
 
     /* schedule first timer interrupt */
     timer_setTimer(TIMER_INTERVAL, timer, NULL);

src/pim_proto.c
@@ -235,9 +235,6 @@ int receive_pim_hello(uint32_t src, uint32_t dst __attribute__((unused)), char *
      * Need to trigger joins?
      */
 
-    IF_DEBUG(DEBUG_PIM_HELLO)
-	dump_vifs(stderr);      /* Show we got a new neighbor */
-
     return TRUE;
 }
 

src/timer.c
@@ -188,11 +188,6 @@ void age_vifs(void)
 	    }
 	}
     }
-
-    IF_DEBUG(DEBUG_IF) {
-	fputs("\n", stderr);
-	dump_vifs(stderr);
-    }
 }
 
 #define MRT_IS_LASTHOP(mrt) PIMD_VIFM_LASTHOP_ROUTER(mrt->leaves, mrt->oifs)
@@ -779,11 +774,6 @@ void age_routes(void)
 	for (nbr = v->uv_pim_neighbors; nbr; nbr = nbr->next)
 	    pack_and_send_jp_message(nbr);
     }
-
-    IF_DEBUG(DEBUG_PIM_MRT) {
-	fputs("\n", stderr);
-	dump_pim_mrt(stderr);
-    }
 }
 
 
@@ -852,10 +842,6 @@ void age_misc(void)
 	    }
 	}
     }
-
-    IF_DEBUG(DEBUG_PIM_BOOTSTRAP | DEBUG_PIM_CAND_RP)
-	dump_rp_set(stderr);
-    /* TODO: XXX: anything else to timeout */
 }
 
 /**