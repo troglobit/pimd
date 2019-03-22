troglobit/pimd

commit 15d0261f1728cbd4d1095a680e39e288bfc5dd42

Fix #142: Skip register_vif in 'pimctl show igmp interface'
The register_vif is never used to send/receive IGMP messages,
it should always be skipped in all listings, same as pimd.

Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/ipc.c
@@ -458,6 +458,10 @@ static void show_igmp_iface(FILE *fp, int detail)
 		char timeout[10];
 		int version;
 
+		/* The register_vif is never used for IGMP messages */
+		if (v->uv_flags & VIFF_REGISTER)
+			continue;
+
 		if (!uv->uv_querier) {
 			strlcpy(s1, "Local", sizeof(s1));
 			snprintf(timeout, sizeof(timeout), "None");

commit 1f3784f7f87f7a430cd30cb4df43a8e382a6cf99

Fix spelling error introduced in 15d0261
Signed-off-by: Joachim Nilsson <troglobit@gmail.com>

src/ipc.c
@@ -459,7 +459,7 @@ static void show_igmp_iface(FILE *fp, int detail)
 		int version;
 
 		/* The register_vif is never used for IGMP messages */
-		if (v->uv_flags & VIFF_REGISTER)
+		if (uv->uv_flags & VIFF_REGISTER)
 			continue;
 
 		if (!uv->uv_querier) {